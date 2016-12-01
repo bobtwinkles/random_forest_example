2107ca31fa6f22e36d165859896b335a7941da76
bobtwinkles <srkoser+github@gmail.com>
2016-12-01 15:03:18 -0500

    Fix iris plots

--- iris_plot.R
+++ iris_plot.R
@@ -1,13 +1,40 @@
 library(ggplot2)
+library(gridExtra)
+library(grid)
+
+grid_arrange_shared_legend <- function(..., ncol = length(list(...)), nrow = 1, position = c("bottom", "right")) {
+
+  plots <- list(...)
+  position <- match.arg(position)
+  g <- ggplotGrob(plots[[1]] + theme(legend.position = position))$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  lheight <- sum(legend$height)
+  lwidth <- sum(legend$width)
+  gl <- lapply(plots, function(x) x + theme(legend.position="none"))
+  gl <- c(gl, ncol = ncol, nrow = nrow)
+
+  combined <- switch(position,
+                     "bottom" = arrangeGrob(do.call(arrangeGrob, gl),
+                                            legend,
+                                            ncol = 1,
+                                            heights = unit.c(unit(1, "npc") - lheight, lheight)),
+                     "right" = arrangeGrob(do.call(arrangeGrob, gl),
+                                           legend,
+                                           ncol = 2,
+                                           widths = unit.c(unit(1, "npc") - lwidth, lwidth)))
+  grid.newpage()
+  grid.draw(combined)
+
+}
+
 data(iris)
 cmd.args <- commandArgs(trailingOnly=T)
 fname <- cmd.args[1]
-par(mfrow=c(2,2))
-png(filename=fname, width=6,height=6,units="in", res=300)
-for (width in c("Sepal.Width", "Petal.Width")) {
-  for (length in c("Sepal.Length", "Petal.Length")) {
-    ggplot(iris, aes_string(x=width, y=length, color="Species")) +
-      geom_point()
-  }
-}
-dev.off()
+png(filename=fname, width=10,height=6,units="in", res=300)
+widths <- c("Sepal.Width", "Petal.Width")
+heights <- c("Sepal.Length", "Petal.Length")
+p1 <- ggplot(iris, aes_string(x="Sepal.Width", y="Sepal.Length", color="Species")) + geom_point()
+p2 <- ggplot(iris, aes_string(x="Sepal.Width", y="Petal.Length", color="Species")) + geom_point()
+p3 <- ggplot(iris, aes_string(x="Petal.Width", y="Sepal.Length", color="Species")) + geom_point()
+p4 <- ggplot(iris, aes_string(x="Petal.Width", y="Petal.Length", color="Species")) + geom_point()
+grid_arrange_shared_legend(p1, p2, p3, p4, nrow=2,ncol=2, position="right")
