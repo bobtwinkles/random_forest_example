615722ec801e7c845035707d668c0679a67f83d9
bobtwinkles <srkoser+github@gmail.com>
2016-12-09 15:21:25 -0500

    Adjust label sizes on misclassification plots

--- plot_errors.R
+++ plot_errors.R
@@ -19,4 +19,6 @@ fname <- cmd.args[1]
 png(filename=fname, width=10,height=6,units="in", res=300)
 
 ggplot(data, aes(x=method, y=rate)) +
-  geom_bar(stat="identity")
+  geom_bar(stat="identity") +
+  theme(axis.text=element_text(size=16)) +
+  theme(axis.title=element_text(size=16))
